5c22feb52e4cb9f05084a92555c69716c0ff1896
Nazar <pavlencko.nazar2017@gmail.com>
2022-04-19 18:15:39 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+"use strict";
+
+//const storeName = 'ATB';
+//const storeDescription = {
+ //   budget: 10000,
+ //   employees: [
+ //       'John', 'Nazar', 'Lena'
+ //   ],
+ //   products: {
+ //       cucumber: 10,
+ //       water: 15
+ //   },
+ //   open: true,
+// };
+
+// console.log (storeDescription.employees[2]);
+
+
+//let number = 10,
+//    number2 = '10';
+
+
+//console.log (2 + 2 * 2 == '6');
+
+
+const arr = prompt ('Ти тут?');
